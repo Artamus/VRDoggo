a985db21cc73ebd7406a35203880ac5745f1859a
Lauri Kongas <lauri.kongas@hotmail.com>
2018-06-09 20:03:23 +0300

    Add projected mouse position tracking and camera controls

---
+++ Assets/Skropts/CameraOrbit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrbit : MonoBehaviour {
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    private Vector3 offset;
+
+    void Start() {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate() {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Horizontal") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
--- Assets/Skropts/PlayerController.cs
+++ Assets/Skropts/PlayerController.cs
@@ -15,11 +15,23 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate()
     {
+		/*
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+        */
+
+		Plane plane = new Plane(Vector3.up, transform.position);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float distance = 100;
+		if (plane.Raycast (ray, out distance)) {
+			Vector3 hitPoint = ray.GetPoint (distance);
+			Vector3 projectionPoint = new Vector3 (hitPoint.x, 0.0f, hitPoint.z);
+			Vector3 relativePosition = projectionPoint - transform.position;
+			rb.AddForce (relativePosition);
+		}
     }
 }
